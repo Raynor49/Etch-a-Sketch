154b33282260daad9fa46fa1fe774ac64d73f33e
WILLIS <willisroberts49@gmail.com>
2024-07-02 17:18:46 -0400

    fix js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,15 @@
+const outerdiv = document.createElement("div");
 for (let i = 0; i < 16; i++) {
     console.log("test");
-    if((i+1)%4==0){
-
-    }
     const newdiv = document.createElement("div");
     newdiv.innerHTML = "hello world";
     newdiv.className="innerDiv"
-    document.getElementById("container").appendChild(newdiv);
+
+
+    if ((i + 1) % 4 == 0) {
+        const outerdiv = document.createElement("div");
+        outerdiv.appendChild(newdiv);
+    }
+    outerdiv.className = "outerdiv";
+    document.getElementById("container").appendChild(outerdiv);
 }
