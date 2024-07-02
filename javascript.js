188888ea6cd7208d0cfe1f87b1bbd29160dff0dc
WILLIS <willisroberts49@gmail.com>
2024-07-02 18:16:01 -0400

    working hover

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 
 for (let i = 0; i < 16; i++) {
     let outerdiv = document.createElement("div");
-
+    outerdiv.className="outer";
     for (let j=0; j<16; j++){
         let innerdiv = document.createElement("div");
 
-        innerdiv.innerHTML = "test"
+        innerdiv.innerHTML = "test";
+        innerdiv.className = 'inner';
         outerdiv.appendChild(innerdiv);
     }
     document.getElementById('container').appendChild(outerdiv);
