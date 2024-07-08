95a960d8a9462630f77d6255aabe9672448e124d
WILLIS <willisroberts49@gmail.com>
2024-07-08 17:48:25 -0400

    functioning etch-sketch in same size pixels

--- javascript.js
+++ javascript.js
@@ -1,17 +1,37 @@
+function buildBox(size) {
 
-for (let i = 0; i < 16; i++) {
-    let outerdiv = document.createElement("div");
-    outerdiv.className="outer";
-    for (let j=0; j<16; j++){
-        let innerdiv = document.createElement("div");
+    for (let i = 0; i < size; i++) {
+        let outerdiv = document.createElement("div");
+        outerdiv.className = "outer";
+        for (let j = 0; j < size; j++) {
+            let innerdiv = document.createElement("div");
 
-        // innerdiv.innerHTML = "test";
-        innerdiv.className = 'inner';
-        outerdiv.appendChild(innerdiv);
+            // innerdiv.innerHTML = "test";
+            innerdiv.className = 'inner';
+            innerdiv.id = i;
+            innerdiv.style.height = (950/size) + 'px';
+            innerdiv.style.width = (950/size) + 'px';
+            outerdiv.appendChild(innerdiv);
+            innerdiv.onmouseover = function () {
+                changeColor(innerdiv);
+            }
+        }
+        document.getElementById('container').appendChild(outerdiv);
     }
-    document.getElementById('container').appendChild(outerdiv);
 }
 
-function msg() {
+function changeColor(element) {
+    element.style.backgroundColor = 'red';
+}
+
+function start() {
+    let container = document.getElementById('container');
+    container.innerHTML = '';
+
     let input = prompt("Please enter size");
+    if(input > 100){
+        input = 100;
+    }
+    console.log(input);
+    buildBox(input);
 }
